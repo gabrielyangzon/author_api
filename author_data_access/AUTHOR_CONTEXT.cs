7e39e9f0e8080453fb8d9ca43791b391b87bdee1
gabrielyangzon <gab.yangzon@yahoo.com>
2023-06-27 20:58:40 +0900

    Add project files.

---
+++ author_data_access/AUTHOR_CONTEXT.cs
@@ -0,0 +1,18 @@
+﻿using author_data_types.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace author_data_access
+{
+    public class AuthorContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase(databaseName: "AuthorDB") ;
+        }
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+    }
+}
